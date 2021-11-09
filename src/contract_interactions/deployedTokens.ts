e967e0f05e6bfd3e1d53ca238e1a5a401f3f68ba
Balázs Csete <csetebalu@gmail.com>
2021-11-09 14:08:22 +0100

    multicall deployed tokens

--- src/contract_interactions/deployedTokens.ts
+++ src/contract_interactions/deployedTokens.ts
@@ -1,16 +1,25 @@
-import { contractsByDeployer, numOfDeployedContracts } from "./airdrop"
+import { aggregate } from "@makerdao/multicall"
+import { Chains } from "connectors"
+import { AirdropAddresses, multicallConfigs } from "contracts"
+import { numOfDeployedContracts } from "./airdrop"
 
 const deployedTokens = async (
   chainId: number,
   address: string
 ): Promise<string[]> => {
   const numberOfTokens = await numOfDeployedContracts(chainId, address)
-  const tokenAddresses = await Promise.all(
-    [...Array(+numberOfTokens)].map((_, index) =>
-      contractsByDeployer(chainId, address, index)
-    )
-  )
-  return tokenAddresses
+
+  const requests = [...Array(+numberOfTokens)].map((_, index) => ({
+    target: AirdropAddresses[Chains[chainId]],
+    call: ["contractsByDeployer(address,uint256)(address)", address, index],
+    returns: [[index]],
+  }))
+
+  const {
+    results: { original },
+  } = await aggregate(requests, multicallConfigs[Chains[chainId]])
+
+  return Object.values(original)
 }
 
 export default deployedTokens
--- src/contracts.ts
+++ src/contracts.ts
@@ -4,6 +4,17 @@ import { RPC, supportedChains } from "connectors"
 import AIRDROP_ABI from "static/abis/airdrop.json"
 import ROLE_TOKEN_ABI from "static/abis/roletoken.json"
 
+const multicallConfigs = {
+  GOERLI: {
+    multicallAddress: "0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e",
+    rpcUrl: RPC.GOERLI.rpcUrls[0],
+  },
+  POLYGON: {
+    multicallAddress: "0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507",
+    rpcUrl: RPC.POLYGON.rpcUrls[0],
+  },
+}
+
 enum AirdropAddresses {
   GOERLI = "0x254EF5F30A7260dC9132CFbD4cF9663Ef106b10A",
   POLYGON = "0x18Bb4142B25d39d07b0dd1aAF317D6A963AFdAA8", // TODO: Update this to new contract
@@ -33,5 +44,5 @@ const tokenContractGetters = Object.fromEntries(
   ])
 )
 
-export { AirdropAddresses, tokenContractGetters }
+export { AirdropAddresses, tokenContractGetters, multicallConfigs }
 export default airdropContracts
