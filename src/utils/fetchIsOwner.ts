f8c04bacc076ee34ddfcba7eebe924e3565973c6
Balázs Csete <csetebalu@gmail.com>
2021-12-16 21:05:52 +0100

    cleanup

--- src/utils/fetchIsOwner.ts
+++ src/utils/fetchIsOwner.ts
@@ -1,14 +1,10 @@
-const fetchIsOwner = (serverId: string, discordId: string): Promise<boolean> => {
-  console.log(
-    `${process.env.NEXT_PUBLIC_BACKEND_API}/owner/${serverId}/${discordId}`
-  )
-  return fetch(
+const fetchIsOwner = (serverId: string, discordId: string): Promise<boolean> =>
+  fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_API}/owner/${serverId}/${discordId}`
   ).then((response) =>
     response.ok
       ? response.json()
       : Promise.reject(Error("Failed to fetch server owner"))
   )
-}
 
 export default fetchIsOwner
