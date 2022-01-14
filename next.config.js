be6e80dd9ee4665dae7cefa4f23956784c48823d
valid <valid@zgen.hu>
2022-01-14 18:26:01 +0100

    redirects hotfix

--- next.config.js
+++ next.config.js
@@ -25,10 +25,12 @@ module.exports = {
       {
         source: "/js/script.js",
         destination: "https://stat.zgen.hu/js/plausible.js",
+        permanent: true,
       },
       {
         source: "/api/event",
         destination: "https://stat.zgen.hu/api/event",
+        permanent: true,
       },
     ]
   },
